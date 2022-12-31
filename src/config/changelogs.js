38bdd996da20f82dc82c4a6d4d4cdff33270ff63
Zodiak381 <121726488+Zodiak381@users.noreply.github.com>
2022-12-31 17:04:00 +0100

    Add files via upload

---
+++ src/config/changelogs.js
@@ -0,0 +1,28 @@
+module.exports = {
+    release: "11-1-2022",
+    bugs : [
+        "Meilleur système de musique",
+        "Meilleure configuration des robots",
+        "Correction des cadeaux",
+        "Un nouveau système de suivi des invitations",
+        "Correction du bug des journaux des chaînes",
+        "Meilleure gestion des erreurs pour les commandes"
+    ],
+    features : [
+        "Bot complètement dans les commandes slash",
+        "Nouvelles activités",
+        "Nouvelles statistiques du serveur",
+        "Configurations automatiques",
+        "Journaux d'événements planifiés",
+        "La commande Mute est maintenant une commande timeout",
+        "Améliorations de la mise en page d'intégration",
+        "Un constructeur d'embed avancé",
+        "Commandes de snipe supprimées",
+        "Commandes inutiles supprimées",
+        "Certaines commandes réarrangées",
+        "**Bot 1 year pack** disponible pour **10 Dcredits**",
+        "Meilleure gestion des erreurs pour les commandes"
+    ],
+};
+
+ 
